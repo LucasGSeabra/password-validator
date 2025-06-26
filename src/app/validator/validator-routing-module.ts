1137b04aa3b6f1aa81dddca913a83de47427363b
Lucas <lucasgseabra@gmail.com>
2025-06-26 07:43:16 -0300

    Chore: Configura rotas

--- src/app/validator/validator-routing-module.ts
+++ src/app/validator/validator-routing-module.ts
@@ -1,10 +1,11 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Form } from './form/form';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: Form }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class ValidatorRoutingModule { }
+export class ValidatorRoutingModule {}
