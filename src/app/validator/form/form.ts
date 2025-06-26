cfc9a81a5a2774460b4a8e12a54116b892ae0cc8
Lucas <lucasgseabra@gmail.com>
2025-06-26 17:37:22 -0300

    Chore: Cria lógica para formulário

--- src/app/validator/form/form.ts
+++ src/app/validator/form/form.ts
@@ -1,11 +1,32 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+} from '@angular/forms';
+
+interface FormData {
+  senha: FormControl<string>;
+}
 
 @Component({
   selector: 'app-form',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './form.html',
-  styleUrl: './form.scss'
+  styleUrl: './form.scss',
 })
 export class Form {
+  form: FormGroup<FormData>;
 
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group<FormData>({
+      senha: this.fb.control<string>('', { nonNullable: true }),
+    });
+  }
+
+  onSubmit(): void {
+    const dados = this.form.value;
+  }
 }
