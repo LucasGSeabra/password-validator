be6f3547766a9980993e6d4c74e119e956fe985e
Lucas <lucasgseabra@gmail.com>
2025-06-26 22:34:24 -0300

    Chore: Cria html do componente form

--- src/app/validator/form/form.ts
+++ src/app/validator/form/form.ts
@@ -5,7 +5,12 @@ import {
   FormControl,
   FormGroup,
   ReactiveFormsModule,
+  Validators,
 } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCard } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 interface FormData {
   senha: FormControl<string>;
@@ -13,7 +18,14 @@ interface FormData {
 
 @Component({
   selector: 'app-form',
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCard
+  ],
   templateUrl: './form.html',
   styleUrl: './form.scss',
 })
@@ -22,11 +34,15 @@ export class Form {
 
   constructor(private fb: FormBuilder) {
     this.form = this.fb.group<FormData>({
-      senha: this.fb.control<string>('', { nonNullable: true }),
+      senha: this.fb.control<string>('', {
+        nonNullable: true,
+        validators: [Validators.required],
+      }),
     });
   }
 
   onSubmit(): void {
     const dados = this.form.value;
+    console.log(dados)
   }
 }
